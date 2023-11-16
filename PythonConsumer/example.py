07709d246506464a30f326026cc47e1d3010bd61
Anton <antje12@student.sdu.dk>
2023-11-16 15:47:20 +0100

    message formatting in prototype

--- PythonConsumer/example.py
+++ PythonConsumer/example.py
@@ -4,5 +4,6 @@ from datetime import datetime
 consumer = KafkaConsumer('foo', bootstrap_servers=['localhost:19092'], group_id='group1')
 
 for msg in consumer:
+    message_value = msg.value.decode('utf-8')  # Assuming UTF-8 encoding
     timestamp_with_ms = datetime.now().strftime('%d-%m-%Y %H:%M:%S.%f')[:-3]
-    print ("Message: ", msg.value, " received at ", timestamp_with_ms)
+    print ("Message: ", message_value, " received at ", timestamp_with_ms)

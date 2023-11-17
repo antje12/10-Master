3cb694db8c04f2ff0545fba2511c7bccc9a9689f
Anton <antje12@student.sdu.dk>
2023-11-17 14:32:53 +0100

    main method introduced

--- KafkaProducerApp/KafkaConsumerApp/Program.cs
+++ KafkaProducerApp/KafkaConsumerApp/Program.cs
@@ -1,29 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
 
-using Confluent.Kafka;
-
-Console.WriteLine("Hello, World!");
-
-ConsumerConfig _consumerConfig;
-
-//const string KafkaServers = "kafka-1:9092,kafka-2:9092,kafka-3:9092"; // internal docker call
-const string KafkaServers = "localhost:19092"; //,localhost:29092,localhost:39092"; // external docker call
-const string GroupId = "msg-group";
-//const string SchemaRegistry = "http://schema-registry:8081"; // internal docker call
-
-_consumerConfig = new ConsumerConfig
+class Program
 {
-    BootstrapServers = KafkaServers,
-    GroupId = GroupId,
-    AutoOffsetReset = AutoOffsetReset.Earliest
-};
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
 
-using var consumer = new ConsumerBuilder<string, string>(_consumerConfig).Build();
-consumer.Subscribe("msg-topic");
-while (true)
+        ConsumerConfig consumerConfig;
+
+        const string KafkaServers = "localhost:19092";
+        const string GroupId = "msg-group";
+
+        consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = KafkaServers,
+            GroupId = GroupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        var data = new Dictionary<string, Location>();
+
+        using var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build();
+        consumer.Subscribe("msg-topic");
+
+        while (true)
+        {
+            var consumeResult = consumer.Consume();
+            var key = consumeResult.Message.Key;
+            var message = consumeResult.Message.Value;
+
+            if (!data.ContainsKey(key))
+            {
+                data.Add(key, new Location()
+                {
+                    x = 0,
+                    y = 0
+                });
+            }
+
+            switch (message)
+            {
+                case "w":
+                    data[key].y++;
+                    break;
+                case "a":
+                    data[key].x--;
+                    break;
+                case "s":
+                    data[key].y--;
+                    break;
+                case "d":
+                    data[key].x++;
+                    break;
+            }
+
+            Console.Clear();
+            Console.Write($"{key}: {data[key].x},{data[key].y}");
+        }
+    }
+}
+
+public class Location
 {
-    var consumeResult = consumer.Consume();
-    var result = consumeResult.Message.Value;
-    Console.WriteLine($"{result} consumed - {DateTime.Now.ToString("dd/MM/yyyy HH.mm.ss.fff")}");
-    //consumer.Close();
+    public int x { get; set; }
+    public int y { get; set; }
 }
--- KafkaProducerApp/KafkaProducerApp/Program.cs
+++ KafkaProducerApp/KafkaProducerApp/Program.cs
@@ -1,31 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
 
-using Confluent.Kafka;
-
-Console.WriteLine("Hello, World!");
-
-ProducerConfig _producerConfig;
-
-//const string KafkaServers = "kafka-1:9092,kafka-2:9092,kafka-3:9092"; // internal docker call
-const string KafkaServers = "localhost:19092"; //,localhost:29092,localhost:39092"; // external docker call
-//const string SchemaRegistry = "http://schema-registry:8081"; // internal docker call
-
-_producerConfig = new ProducerConfig
+class Program
 {
-    BootstrapServers = KafkaServers,
-    Acks = Acks.None,
-    LingerMs = 0,
-    BatchSize = 1
-};
-
-using var producer = new ProducerBuilder<Null, string>(_producerConfig).Build();
-while (true)
-{
-    string message = Console.ReadLine();
-    Console.WriteLine($"{message} produced - {DateTime.Now.ToString("dd/MM/yyyy HH.mm.ss.fff")}");
-    producer.Produce("msg-topic", new Message<Null, string>
+    static void Main()
     {
-        Value = message
-    });
-    producer.Flush();
+        Console.WriteLine("Hello, World!");
+
+        ProducerConfig producerConfig;
+
+        const string KafkaServers = "localhost:19092";
+        
+        producerConfig = new ProducerConfig
+        {
+            BootstrapServers = KafkaServers,
+            Acks = Acks.None,
+            LingerMs = 0,
+            BatchSize = 1
+        };
+
+        using var producer = new ProducerBuilder<string, string>(producerConfig).Build();
+        
+        while (true)
+        {
+            string message = Console.ReadLine();
+            Console.WriteLine($"{message} produced - {DateTime.Now.ToString("dd/MM/yyyy HH.mm.ss.fff")}");
+            
+            producer.Produce("msg-topic", new Message<string, string>
+            {
+                Key = "myPlayer",
+                Value = message
+            });
+            
+            producer.Flush();
+        }
+    }
 }

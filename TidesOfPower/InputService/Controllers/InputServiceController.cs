fbf0322d4d82fd5876e60c40ebbeef81f7d331e6
Anton <antje12@student.sdu.dk>
2024-02-05 15:24:42 +0100

    add input service

---
+++ TidesOfPower/InputService/Controllers/InputServiceController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InputService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InputServiceController : ControllerBase
+{
+    private const string _version = "1.00";
+
+    [HttpGet("Version")]
+    public object Version()
+    {
+        return new {Version = _version};
+    }
+
+    [HttpGet("Status")]
+    public object Status()
+    {
+        return true;
+    }
+
+    [HttpGet("Start")]
+    public object Start()
+    {
+        return true;
+    }
+
+    [HttpGet("Stop")]
+    public object Stop()
+    {
+        return true;
+    }
+}

b2a566656ccb7a17c7f92d0304f9365a77263487
Anton <antje12@student.sdu.dk>
2024-03-15 12:26:14 +0100

    optimized pipeline

--- TidesOfPower/GameClient/Entities/Player.cs
+++ TidesOfPower/GameClient/Entities/Player.cs
@@ -14,7 +14,7 @@ public class Player : Agent
 {
     private MyGame _game;
     public int Latency = 0;
-    public int Health = 0;
+    public int Health = 100;
     public int Score = 0;
     
     public Vector2 MousePosition { get; set; }

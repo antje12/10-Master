1650d7160aaf91fbf2eb2a4cb574f2e783b7a7a4
Anton <antje12@student.sdu.dk>
2024-05-01 11:11:38 +0200

    test

--- TidesOfPower/TestConsole/Tests/Latency.cs
+++ TidesOfPower/TestConsole/Tests/Latency.cs
@@ -4,16 +4,37 @@ public class Latency
 {
     public async Task Test()
     {
+        var _path = @"D:\Git\10-Master\Experiments\Scalability.txt";
+        File.Delete(_path);
+
         var tasks = new List<Task>();
         var numberOfClients = 10;
         var numberOfSteps = 100;
+        var index = 0;
+        while (true)
+        {
+            AddBatch(index, numberOfClients, numberOfSteps, tasks);
+            index += 10;
 
-        for (int i = 0; i < numberOfClients; i++)
+            var timestamp = DateTime.UtcNow;
+            using (StreamWriter file = File.AppendText(_path))
+            {
+                file.WriteLine($"{timestamp:o} - Added 10 - {tasks.Count} clients running");
+            }
+
+            Console.WriteLine($"Added 10 - {tasks.Count} clients running");
+            Thread.Sleep(30000);
+        }
+
+        //await Task.WhenAll(tasks);
+    }
+
+    private static void AddBatch(int index, int numberOfClients, int numberOfSteps, List<Task> tasks)
+    {
+        for (int i = index; i < index + numberOfClients; i++)
         {
             var client = new KafkaLatencyClient(i, numberOfSteps);
             tasks.Add(Task.Run(() => client.Test()));
         }
-
-        await Task.WhenAll(tasks);
     }
 }

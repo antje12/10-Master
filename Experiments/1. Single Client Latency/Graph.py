786caad63e112b0d4f88dde831919483b1e7038b
Anton <antje12@student.sdu.dk>
2024-05-02 12:36:27 +0200

    wip

--- Experiments/1. Single Client Latency/Graph.py
+++ Experiments/1. Single Client Latency/Graph.py
@@ -1,46 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Path to the CSV file
 file_path = 'Latency.csv'
-
-# Read the CSV file
 df = pd.read_csv(file_path, sep=';', header=None, names=['timestamp', 'latency'])
-
-# Convert the timestamp column to datetime
+# Convert to datetime
 df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-# Set the timestamp column as the index
+# Set timestamp as index
 df.set_index('timestamp', inplace=True)
 
-# Check the spread of data points
-print("Timestamp frequency stats:")
-print(df.index.to_series().diff().describe())
-
-# Resample the data by millisecond and calculate the mean of latency
-df_resampled = df.resample('S').mean()  # 'L' stands for millisecond
-
-# Interpolating missing values
+# Resample the data by second and calculate latency mean
+df_resampled = df.resample('S').mean()  # S = sec, L = ms
+# Interpolate missing values
 df_resampled.interpolate(method='linear', inplace=True)
-
-# Reset index to use timestamp as a column again
+# Reset index
 df_resampled.reset_index(inplace=True)
 
-# Plotting the data
 plt.figure(figsize=(10, 5))
-if not df_resampled['latency'].isna().all():  # Check if all values are NaN
-    plt.plot(df_resampled['timestamp'], df_resampled['latency'], label='Average Latency per Millisecond')
-    plt.title('Average Latency Evolution Over Time by Millisecond')
-    plt.xlabel('Time')
-    plt.ylabel('Latency (ms)')
-    plt.grid(True)
-    plt.xticks(rotation=45)
+plt.plot(df_resampled['timestamp'], df_resampled['latency'], label='Average Latency per Millisecond')
+plt.title('Average Latency Evolution Over Time by Millisecond')
+plt.xlabel('Time')
+plt.ylabel('Latency (ms)')
+plt.grid(True)
+plt.xticks(rotation=45)
 
-    # Add a horizontal line at 150 ms
-    plt.axhline(y=150, color='blue', linestyle='--', label='Threshold at 150 ms')
+# Add a horizontal line at 150 ms
+plt.axhline(y=150, color='blue', linestyle='--', label='Threshold at 150 ms')
 
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-else:
-    print("All resampled data points are NaN. Check the original data density and time spread.")
+plt.legend()
+plt.tight_layout()
+plt.show()
--- Experiments/3. Two Instance Latency Load/Graph.py
+++ Experiments/3. Two Instance Latency Load/Graph.py
@@ -1,51 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Path to the CSV file
 file_path = 'Latency/Client0_Latency.csv'
-
-# Read the CSV file
 df = pd.read_csv(file_path, sep=';', header=None, names=['timestamp', 'latency'])
-
-# Convert the timestamp column to datetime
+# Convert to datetime
 df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-# Set the timestamp column as the index
+# Set timestamp as index
 df.set_index('timestamp', inplace=True)
 
-# Resample the data by millisecond and calculate the mean of latency
-df_resampled = df.resample('S').mean()  # 'L' stands for millisecond
-
-# Interpolating missing values
+# Resample the data by second and calculate latency mean
+df_resampled = df.resample('S').mean()  # S = sec, L = ms
+# Interpolate missing values
 df_resampled.interpolate(method='linear', inplace=True)
-
-# Reset index to use timestamp as a column again
+# Reset index
 df_resampled.reset_index(inplace=True)
 
-# Read timestamps from Scalability.txt
+# Read timestamps
 timestamps_path = 'Scalability.txt'
 with open(timestamps_path, 'r') as file:
     timestamps = [pd.to_datetime(line.strip()) for line in file.readlines()]
 
-# Plotting the data
 plt.figure(figsize=(10, 5))
-if not df_resampled['latency'].isna().all():  # Check if all values are NaN
-    plt.plot(df_resampled['timestamp'], df_resampled['latency'], label='Average Latency per Millisecond')
-    plt.title('Average Latency Evolution Over Time by Millisecond')
-    plt.xlabel('Time')
-    plt.ylabel('Latency (ms)')
-    plt.grid(True)
-    plt.xticks(rotation=45)
-    
-    # Add a horizontal line at 150 ms
-    plt.axhline(y=150, color='blue', linestyle='--', label='Threshold at 150 ms')
+plt.plot(df_resampled['timestamp'], df_resampled['latency'], label='Average Latency per Millisecond')
+plt.title('Average Latency Evolution Over Time by Millisecond')
+plt.xlabel('Time')
+plt.ylabel('Latency (ms)')
+plt.grid(True)
+plt.xticks(rotation=45)
 
-    # Adding vertical lines from Scalability.txt
-    for specific_time in timestamps:
-        plt.axvline(x=specific_time, color='red', linestyle='--', label='Target Time')
+# Add a horizontal line at 150 ms
+plt.axhline(y=150, color='blue', linestyle='--', label='Threshold at 150 ms')
 
-    #plt.legend()
-    plt.tight_layout()
-    plt.show()
-else:
-    print("All resampled data points are NaN. Check the original data density and time spread.")
+# Adding vertical lines from Scalability.txt
+for specific_time in timestamps:
+    plt.axvline(x=specific_time, color='red', linestyle='--', label='Target Time')
+
+plt.legend()
+plt.tight_layout()
+plt.show()

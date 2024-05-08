45b1d7e90342538167e09faef84ef3f90e2a5165
Anton <antje12@student.sdu.dk>
2024-05-08 14:39:19 +0200

    wip

---
+++ Experiments/Results/N2.1 Baseline Latency test/Boxplot.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = 'Latency.csv'
+df = pd.read_csv(file_path, sep=';', header=None, names=['timestamp', 'latency'])
+# Convert to datetime
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+# Set timestamp as index
+df.set_index('timestamp', inplace=True)
+
+latency = df['latency']  # Extract the 'latency' column
+
+print("Summary statistics for Latency:")
+print("Min:", np.min(latency))
+print("1st Quartile:", np.percentile(latency, 25))
+print("Median:", np.median(latency))
+print("Mean:", np.mean(latency))
+print("3rd Quartile:", np.percentile(latency, 75))
+print("Max:", np.max(latency))
+
+plt.boxplot([latency], labels=["Client"])
+#plt.title("Latency")
+#plt.xlabel("Data Sets")
+plt.ylabel("Latency")
+plt.show()
